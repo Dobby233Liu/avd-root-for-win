a0d4fce0fd26cb857a6044aab00e5c57d88281fe
Dobby233Liu <15816141883@163.com>
2018-12-31 20:32:14 +0800

    fixes

--- avd-root.bat
+++ avd-root.bat
@@ -1,12 +1,12 @@
 @echo off
-set %ARCH%=%1
+cd /d %~dp0
 adb root
-adb push ./bb/%ARCH%/busybox /data/local/busybox
+adb push .\bb\%1\busybox /data/local/busybox
 adb shell "chmod 755 /data/local/busybox"
 adb shell "/data/local/busybox mount -o rw,remount /system"
 adb shell "/data/local/busybox --install -s /system/xbin"
 adb shell "rm -r /system/media/audio"
-adb push ./su/%ARCH%/bin/su /system/bin/su
+adb push .\su\%1\bin\su /system/bin/su
 adb shell "chown 0:0 /system/bin/su"
 adb shell "chmod 6755 /system/bin/su"
-adb install ./su/%ARCH%/app/Superuser.apk
+adb install .\su\%1\app\Superuser.apk
